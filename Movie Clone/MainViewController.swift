3a549fcb75f033a87eae8a0f3db04cab334e45be
Alwi <wishihab@gmail.com>
2022-09-23 09:50:06 +0700

    Membuat TabViewController beranda, bioskop, tiket

--- Movie Clone/MainViewController.swift
+++ Movie Clone/MainViewController.swift
@@ -13,6 +13,21 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        
+        let berandaView = UINavigationController(rootViewController: BerandaViewController())
+        let bioskopView = UINavigationController(rootViewController: BioskopViewController())
+        let tiketView = UINavigationController(rootViewController: TiketViewController())
+        
+        berandaView.tabBarItem.image = UIImage(systemName: "house.fill")
+        bioskopView.tabBarItem.image = UIImage(systemName: "building.2.fill")
+        tiketView.tabBarItem.image = UIImage(systemName: "ticket.fill")
+        
+        berandaView.title = "Beranda"
+        bioskopView.title = "Bioskop"
+        tiketView.title = "Tiket"
+        
+        tabBar.tintColor = .label
+        setViewControllers([berandaView, bioskopView, tiketView], animated: true)
     }
 
 
