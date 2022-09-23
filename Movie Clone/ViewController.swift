5d3a8bf29faf5d7cbfde5a3fc780c4e884715aa3
Alwi <wishihab@gmail.com>
2022-09-23 09:11:46 +0700

    Initial Commit

---
+++ Movie Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Clone
+//
+//  Created by Alwi Shihab on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
